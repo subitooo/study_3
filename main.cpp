f497b9c7b45d91b488c871d0c77de8d9fc794802
subitooo <ishkaeva.vika@mail.ru>
2024-05-14 21:40:29 +0200

    CMakeAdded

---
+++ main.cpp
@@ -0,0 +1,16 @@
+﻿#include "func.hpp"
+
+int main()
+{
+	sd::MyString str1("testik");
+	sd::MyString str2("CodeCode");
+	sd::MyString str3 = str2;
+
+	str2 = str1;
+
+	str1.Print();
+	str2.Print();
+	str3.Print();
+
+	return 0;
+}
